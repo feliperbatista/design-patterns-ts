bc828a90502282dcaab2a6545806285a614df759
Felipe Batista <feliperodriguesbatista@outlook.com>
2024-04-02 16:02:59 -0300

    Factory method

---
+++ src/creational/factory-method/client-code.ts
@@ -0,0 +1,16 @@
+import { CarFactory } from './factories/car-factory';
+import { randomCarAlgorithm } from './main/random-vehicle-algorithm';
+import { randomNumbers } from './utils/random-numbers';
+
+const carFactory = new CarFactory();
+const customerNames = ['Felipe', 'Eduardo', 'Mariana', 'Maria'];
+
+for (let i = 0; i < 10; i++) {
+  const vehicle = randomCarAlgorithm();
+  const name = customerNames[randomNumbers(customerNames.length)];
+  vehicle.pickUp(name);
+  vehicle.stop();
+  const newCar = carFactory.pickUp(name, `Novo carro - ${randomNumbers(100)}`);
+  newCar.stop();
+  console.log('------');
+}
