59ed6f9630b5a8ae0f03d2283a078d473101d134
Felipe Batista <feliperodriguesbatista@outlook.com>
2024-04-04 10:02:24 -0300

    Strategy

---
+++ src/behavioural/strategy/index.ts
@@ -0,0 +1,10 @@
+import { DefaultDiscount } from './shopping-cart/default-discount';
+import { ECommerceShoppingCart } from './shopping-cart/ecommerce-shopping-cart';
+
+const shoppingCart = new ECommerceShoppingCart();
+shoppingCart.discount = new DefaultDiscount();
+shoppingCart.addProduct({ name: 'Produto 1', price: 50 });
+shoppingCart.addProduct({ name: 'Produto 3', price: 50 });
+shoppingCart.addProduct({ name: 'Produto 4', price: 50 });
+console.log(shoppingCart.getTotal());
+console.log(shoppingCart.getTotalWithDiscount());
