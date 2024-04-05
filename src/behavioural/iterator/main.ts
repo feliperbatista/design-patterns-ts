4be55f867a8174ee13be46c66b3a6aeb60f70002
Felipe Batista <feliperodriguesbatista@outlook.com>
2024-04-05 16:39:13 -0300

    Iterator

---
+++ src/behavioural/iterator/main.ts
@@ -0,0 +1,5 @@
+import { MyDataStructure } from './my-default-structure';
+
+const dataStructure = new MyDataStructure();
+dataStructure.addItem('A', 'B', 'C', 'D', 'E', 'F');
+console.log(dataStructure);
